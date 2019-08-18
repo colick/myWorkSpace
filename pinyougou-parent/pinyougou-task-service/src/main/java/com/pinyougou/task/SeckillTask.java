52d692c0266c8ef29575741a0d9f7bb46dd63930
colick <cioctorwu@163.com>
2019-08-18 18:45:07 +0800

    添加注释

--- pinyougou-parent/pinyougou-task-service/src/main/java/com/pinyougou/task/SeckillTask.java
+++ pinyougou-parent/pinyougou-task-service/src/main/java/com/pinyougou/task/SeckillTask.java
@@ -8,6 +8,10 @@ import java.util.Date;
 @Component
 public class SeckillTask {
 
+    /**
+     * 定时任务测试
+     * @throws Exception
+     */
     @Scheduled(cron = "* * * * * ?")
     public void testTask() throws Exception {
         System.out.println(new Date());
